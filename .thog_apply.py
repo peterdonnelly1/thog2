c94cdb71eb54e8b8857db5afb287f6e3a10c7f1a
Peter Donnelly <pd@red.com.au>
2026-07-22 10:00:50 +1000

    Fix lapped cosine version help patch

--- .thog_apply.py
+++ .thog_apply.py
@@ -921,15 +921,13 @@ ATTENTION_GEOMETRY=""
     )
     replace_once(
         wrapper_path,
-        """                                                     haar_balanced_binary_orthonormal_v1
-  -a ATTENTION_GEOMETRY=${ATTENTION_GEOMETRY:-preset default}
-""",
-        """                                                     haar_balanced_binary_orthonormal_v1
+        """haar_balanced_binary_orthonormal_v1
+  -a ATTENTION_GEOMETRY=${ATTENTION_GEOMETRY:-preset default}""",
+        """haar_balanced_binary_orthonormal_v1
                                                      lapped_cosine_balanced_orthonormal_v1
   -W LAPPED_COSINE_WINDOW_LENGTH=${LAPPED_COSINE_WINDOW_LENGTH}
   -i LAPPED_COSINE_OVERLAP_FRACTION=${LAPPED_COSINE_OVERLAP_FRACTION}  currently 0.5 only
-  -a ATTENTION_GEOMETRY=${ATTENTION_GEOMETRY:-preset default}
-""",
+  -a ATTENTION_GEOMETRY=${ATTENTION_GEOMETRY:-preset default}""",
     )
     replace_once(
         wrapper_path,
