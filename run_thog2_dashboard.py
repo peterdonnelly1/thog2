3b51d384cbe0a6a006f6ed43b979baee34b6c822
Peter Donnelly <pd@red.com.au>
2026-08-22 13:37:34 +1000

    Wire checkpoint segment size column

--- run_thog2_dashboard.py
+++ run_thog2_dashboard.py
@@ -57,6 +57,7 @@ _EXTRA_ASSET_NAMES = (
     "dashboard_matched_weight_workspace_repair_patch.js",
     "dashboard_workspace_stability_performance_patch.js",
     "dashboard_weight_controls_run_table_patch.js",
+    "dashboard_run_table_s_checkpoint_patch.js",
 )
 
 
